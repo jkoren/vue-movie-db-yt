10b4e74c750342ef39be71f8329f84741ec5ede2
jkoren <j.korenstein@gmail.com>
2022-04-24 07:42:35 -0400

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h3>hello there</h3>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
